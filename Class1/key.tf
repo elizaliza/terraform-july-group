b1c8f0824d4ea171a91796bd496a07f7b4d09cad
elizaliza <vidana_999@mail.ru>
2023-09-28 22:04:47 +0000

    new files

--- Class1/key.tf
+++ Class1/key.tf
@@ -15,3 +15,14 @@ resource "aws_key_pair" "deployer" {
 resource "aws_s3_bucket" "example" {
   bucket_prefix = "hello-"
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "manually-created-bucketliza"
+}
+
+output "bucket_info" {
+  value = [
+    aws_s3_bucket.bucket.bucket,
+    aws_s3_bucket.bucket.arn
+  ]
+}
